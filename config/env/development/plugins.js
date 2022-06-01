7e611bd2cca743fab88562abbcdbb77019761b20
renedsoft <renedsoft@gmail.com>
2022-06-01 12:42:58 -0400

    logo

---
+++ config/env/development/plugins.js
@@ -0,0 +1,24 @@
+module.exports = ({ env }) => ({
+  email: {
+    config: {
+      provider: 'mailgun',
+      providerOptions: {
+        apiKey: env('MAILGUN_API_KEY'),
+        domain: env('DOMAIN'), //Required if you have an account with multiple domains
+        host: 'api.mailgun.net', //Optional. If domain region is Europe use 'api.eu.mailgun.net'
+      },
+      settings: {
+        defaultFrom: 'myemail@protonmail.com',
+        defaultReplyTo: 'myemail@protonmail.com',
+      },
+    },
+  },
+  upload: {
+    config: {
+      provider: 'local',
+      providerOptions: {
+        sizeLimit: 100000,
+      },
+    },
+  },
+});
